d14ca45620e4b5072bcfb7492f41d14548e9e0a8
KaiBytes <gjchocopasta@gmail.com>
2021-02-18 14:26:15 +0100

    updated solution

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,14 +1,10 @@
-//Object keyword
-//what is the exact difference between imperative and functional programming??
-//idk what the arrow does?
+
 
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
         if (leftStrand.length == rightStrand.length){
-            var count = 0;
-            leftStrand.zip(rightStrand) { leftStrand, rightStrand -> if (leftStrand != rightStrand) count++ }
-            return count
+             return leftStrand.zip(rightStrand) { leftStrand, rightStrand -> leftStrand != rightStrand }.filter { it == true }.count()
         } else {
             return throw IllegalArgumentException("left and right strands must be of equal length")
         }
